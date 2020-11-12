96f46c89f1ec4e22748d919e5493df12a08f4692
Λ B H I N Λ V <ixd.abhi@gmail.com>
2020-11-12 16:09:29 -0500

    update the code with url adding functionality

--- GreenApple/index.js
+++ GreenApple/index.js
@@ -4,6 +4,8 @@ const fetch = require('node-fetch')
 const app = express()
 const PORT = 3000
 
+let urls = [];
+
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({
     extended: false
@@ -26,26 +28,54 @@ app.use((req, res, next) => {
 
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
 
-const processSomething = callback => {
-    setTimeout(callback, 5000);
-}
-
-app.post("/updateStatus", (req, res) => {
-    console.log("Status", req.body.data);
-    fetch("http://localhost:1000/hook", { // client hook url
-            method: 'POST',
-            body: JSON.stringify({
-                "status": req.body.data
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        }).then(res => res.json())
-        .then(json => {
-            console.log(json);
-            res.status(200).send({
-                status: req.body.data,
-                msg: 'successful'
-            });
-        });
+// add url to url array
+app.post('/addURL', (req, res) => {
+    console.log("URL", req.body.data);
+    if (req.body.data.length>0) {
+        urls.push(req.body.data);
+    }
+    res.status(200).send({
+        data:urls,
+        msg: "Successfully Added"
+    });
 });
+
+// get all urls registered for hook
+app.get('/allURL', (req, res) => {
+    res.status(200).send({
+        data:urls,
+        msg: "Successfully Added"
+    });
+});
+
+// update status
+app.post("/updateStatus", (req, res) => {
+    if (urls.length > 0) {
+        updateURLStatus(req.body.data,res);
+    } else {
+        res.status(200).send({
+            msg: 'no url in the list'
+        });
+    }
+});
+
+// update status to these urls
+function updateURLStatus(status,res) {
+    urls.forEach((val, idx) =>{ 
+            fetch(val, { 
+                    method: 'POST',
+                    body: JSON.stringify({
+                        "status": status
+                    }),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                }).then(data => data.json())
+                .then(json => { 
+                    res.status(200).send({
+                        status: json,
+                        msg: 'successful'
+                    });
+                });
+        });
+}
